c2329b93de954c1d686e92de2cda8b921bde074b
Lewis Walsh <lewis@lewiswalsh.com>
2016-01-12 11:37:27 +0000

    initital commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+
+  var gulp    = require('gulp');
+  var sass    = require('gulp-sass');
+  var cssNano = require('gulp-cssnano');
+  //var uglify    = require('gulp-uglify');
+  //var concat    = require('gulp-concat');
+  var watch   = require('gulp-watch');
+
+  var paths = {
+    src : {
+      sass : './scss/**/*.scss',
+      js   : './public/src/js/**/*.js'
+    },
+    dist : {
+      css : './css',
+      js  : './public/dist/js'
+    }
+  };
+
+  gulp.task('css', function(){
+    return gulp.src(paths.src.sass)
+      .pipe(sass())
+      .pipe(cssNano())
+      .pipe(gulp.dest(paths.dist.css));
+  });
+
+  // gulp.task('js', function(){
+  //   return gulp.src(paths.src.js)
+  //     .pipe(concat('dist.js'))
+  //     .pipe(uglify())
+  //     .pipe(gulp.dest(paths.dist.js));
+  // });
+
+  gulp.task('watch', function(){
+    watch(paths.src.sass, function(){ gulp.start('css'); });
+    //watch(paths.src.js, function(){ gulp.start('js'); });
+  });
+
+  gulp.task('default', ['css', 'watch']); // 'js',  'images',
