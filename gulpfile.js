5f205f9b88c80a6b573cc62b95b2f7d67efc72d5
Lewis Walsh <lewis@lewiswalsh.com>
2016-05-08 11:28:01 +0100

    Removed old remmed out code

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,14 @@
   var sass         = require('gulp-sass');
   var cssNano      = require('gulp-cssnano');
   var autoprefixer = require('gulp-autoprefixer');
-  //var uglify     = require('gulp-uglify');
-  //var concat     = require('gulp-concat');
   var watch        = require('gulp-watch');
 
   var paths = {
     src : {
-      sass : './scss/**/*.scss',
-      js   : './public/src/js/**/*.js'
+      sass : './scss/**/*.scss'
     },
     dist : {
-      css : './css',
-      js  : './public/dist/js'
+      css : './css'
     }
   };
 
@@ -30,16 +26,8 @@
       .pipe(gulp.dest(paths.dist.css));
   });
 
-  // gulp.task('js', function(){
-  //   return gulp.src(paths.src.js)
-  //     .pipe(concat('dist.js'))
-  //     .pipe(uglify())
-  //     .pipe(gulp.dest(paths.dist.js));
-  // });
-
   gulp.task('watch', function(){
     watch(paths.src.sass, function(){ gulp.start('css'); });
-    //watch(paths.src.js, function(){ gulp.start('js'); });
   });
 
-  gulp.task('default', ['css', 'watch']); // 'js',  'images',
+  gulp.task('default', ['css', 'watch']);
